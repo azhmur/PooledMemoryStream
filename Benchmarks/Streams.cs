d73e6807fde0234a96077a88fcaeb7211d809a22
Anatoly Zhmur <Anatoly.Zhmur@broadridge.com>
2023-06-23 15:04:39 +0100

    1.0.4: some dependencies updated.

--- Benchmarks/Streams.cs
+++ Benchmarks/Streams.cs
@@ -2,12 +2,12 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 using BetterStreams;
-using LocalsInit;
 using Microsoft.IO;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -21,11 +21,7 @@ namespace Benchmarks
 
         private byte[] data;
 
-        private byte[] data2;
-
         private RecyclableMemoryStreamManager memoryManager;
-
-        private object o;
 
         [Params(24, 10_000, 100_000)]
         public int Length;
@@ -34,67 +30,7 @@ namespace Benchmarks
         public void GlobalSetup()
         {
             this.data = new byte[this.Length];
-            this.data2 = new byte[this.Length];
             this.memoryManager = new RecyclableMemoryStreamManager();
-        }
-
-        [Benchmark]
-        public void NewComplexObject()
-        {
-            for (int i = 0; i < Length / 24; ++i)
-            {
-                var complex = new Complex();
-                complex.Dispose();
-            }
-        }
-
-        [Benchmark]
-        public void NewObject()
-        {
-            for (int i = 0; i < Length / 24; ++i)
-            {
-                this.o = new object();
-            }
-        }
-
-        [Benchmark]
-        [BenchmarkCategory("Basic")]
-        public void NewArray()
-        {
-            var array = new byte[this.Length];
-        }
-
-        [Benchmark]
-        [BenchmarkCategory("Basic")]
-        public unsafe void Native()
-        {
-            var native = Marshal.AllocHGlobal(this.Length);
-
-            Marshal.FreeHGlobal(native);
-        }
-
-        [Benchmark]
-        [BenchmarkCategory("Basic")]
-        public void StackAlloc()
-        {
-            Span<byte> data = stackalloc byte[this.Length];
-        }
-
-        [Benchmark]
-        [LocalsInit(false)]
-        [BenchmarkCategory("Basic")]
-        public void StackAllocWithoutLocalsInit()
-        {
-            Span<byte> data = stackalloc byte[this.Length];
-        }
-
-        [Benchmark]
-        [BenchmarkCategory("Basic")]
-        public void ArrayPool()
-        {
-            var array = ArrayPool<byte>.Shared.Rent(this.Length);
-
-            ArrayPool<byte>.Shared.Return(array);
         }
 
         [Benchmark]
@@ -154,41 +90,6 @@ namespace Benchmarks
                 {
                     stream.Write(this.data, position, Math.Min(blockSize, this.Length - position));
                 }
-            }
-        }
-
-        [Benchmark()]
-        public void Copy()
-        {
-            Buffer.BlockCopy(this.data, 0, this.data2, 0, this.Length);
-        }
-
-        [Benchmark()]
-        public void CopyByteLoop()
-        {
-            for (int i = 0; i < this.Length; ++i)
-            {
-                this.data2[i] = this.data[i];
-            }
-        }
-
-        [Benchmark(Baseline = true)]
-        [BenchmarkCategory("Basic")]
-        public void Fill()
-        {
-            this.data.AsSpan().Fill(1);
-        }
-
-        private class Complex
-        {
-            public void Dispose()
-            {
-                GC.SuppressFinalize(this);
-            }
-
-            ~Complex()
-            {
-                var i = 1;
             }
         }
     }
